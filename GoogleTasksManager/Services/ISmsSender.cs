1ef3c38439efe9988dc199dc2e395659ed1164f7
Mateusz Mistecki <mateusz.mistecki@outlook.com>
2017-03-13 11:35:06 +0100

    Initial commit.

---
+++ GoogleTasksManager/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleTasksManager.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
